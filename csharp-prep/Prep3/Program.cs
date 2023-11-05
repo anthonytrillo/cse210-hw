a7ec5cb9c6d5a29d66ec2b52497b7c0e7a1cb7dd
anthonytrillo <anthony.trillo.92@gmail.com>
2023-11-04 23:09:00 -0300

    Added Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        bool response = true;
+
+        // Console.Write("What is the magic number? ");
+        // string number = Console.ReadLine();
+        // int magicNumber = int.Parse(number);
+
+        do {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            int guessNumber = int.Parse(guess);
+
+            if (magicNumber > guessNumber) {
+                Console.WriteLine("Higuer");
+            } else if (magicNumber < guessNumber) {
+                Console.WriteLine("Lower");
+            } else {
+                Console.WriteLine("You guess it!");
+                response = false;
+            }
+        } while (response);
     }
 }
